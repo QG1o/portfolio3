6ff3265be7edc6bf0b558ca1ed4c64474d4e3f0f
Georgios Tertlidis <tertlidis@protonmail.com>
2025-07-24 05:13:13 +0200

    new

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -1,11 +1,46 @@
+import * as THREE from 'three';
+
 const HeroLights = () => {
   return (
     <>
-    <spotLight position={[2, 2, 2]}
-    intensity={100}
-    />
-        </>
-  )
-}
+      <spotLight 
+      position={[2, 5, 6]} 
+      intensity={20} 
+      angle={0.8}
+      penumbra={0.2}
+      color="white"      
+      />
+      
+      <spotLight 
+      position={[4, 5, 6]} 
+      intensity={100} 
+      angle={0.2}
+      penumbra={0.5}
+      color="#4cc9f0"      
+      />
+      
+      <spotLight 
+      position={[2, 5, 6]} 
+      intensity={100} 
+      angle={0.2}
+      penumbra={0.2}
+      color="#9d4edd"      
+      />
+      
+      <spotLight 
+      position={[2, 5, 6]} 
+      intensity={100} 
+      angle={0.3}
+      penumbra={0.2}
+      color="#4cc9f0"      
+      />
 
-export default HeroLights
+    <primitive
+      object={new THREE.RectAreaLight('#9d4edd')}
+    />
+
+    </>
+  );
+};
+
+export default HeroLights;
