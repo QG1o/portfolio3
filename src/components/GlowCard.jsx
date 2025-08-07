178550a09475b35fe816cf95d7c345b917f4b19d
QG1o QG1o <219057471+QG1o@users.noreply.github.com>
2025-08-07 23:00:21 +0200

    Update GlowCard.jsx

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -5,8 +5,20 @@ const GlowCard = ({ card, children, index }) => {
 
     const handleMouseMove = (index) => (e) => {
         const card = cardRefs.current[index];
-        if(card) return;
+        if(!card) return;
 
+        // get the mouse position relative to the card
+        const rect = card.getBoundingClientRect();
+        const mouseX = e.clientX - rect.left - rect.width / 2;
+        const mouseY = e.clientY - rect.top - rect.height / 2;
+
+        //calculate the angle from the center of the card
+        let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+
+
+        angle = (angle + 360) % 360;
+
+        card.style.setProperty('--start', angle + 60)
     }
 
 
